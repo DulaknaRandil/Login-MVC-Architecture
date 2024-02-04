a1569f515f9bf83e47b0d23695de4ee7e39ecc3e
DulaknaRandil <randilacreed@gmail.com>
2024-02-04 17:47:44 +0530

    first commit

---
+++ Login/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        loginEntities db = new loginEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user log)
+        {
+           var user = db.users. Where(x=>x.Username == log.Username && x.Password == log.Password ).Count();
+           if (user > 0)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
